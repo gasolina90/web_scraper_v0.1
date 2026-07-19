d20b34ea0391032d8e9a23db66c6197347074813
Erick <moralerick90@gmail.com>
2026-07-18 22:35:30 -0700

    printed main content

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
-result = requests.get("https://www.geeksforgeeks.org/python/python-programming-language-tutorial/")
-print(result.status_code)
-print(result.content)
+res = requests.get("https://www.geeksforgeeks.org/python/python-programming-language-tutorial/")
+# parse the raw HTML content in a clean readable format
+soup = BeautifulSoup(res.content, "html.parser")
+
+# print(soup.prettify())
+# # print(res.content)
+# print(res.status_code)
+
+# Find the main content container
+content = soup.find("div", class_="article--viewer_content")
+if content:
+    for paragraph in content.find_all('p'):
+        print(paragraph.text.strip())
+else:
+    print("No article content found")
