53e4f5fb5241f61f2eb2efba228a4eae53f3e8fa
Erick <moralerick90@gmail.com>
2026-07-19 22:52:00 -0700

    find and display data

--- driver.py
+++ driver.py
@@ -16,5 +16,34 @@ def main():
     # Use a Service object
     service = Service(ChromeDriverManager().install())
 
+    for page in range(1,3):
+        # Init driver
+        driver = webdriver.Chrome(service=service, options=options)
+
+        # Load the URL
+        url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page=%7Bpage%7D"
+        driver.get(url)
+        time.sleep(2)
+
+        # Extract details
+        titles = driver.find_elements(By.CLASS_NAME, "title")
+        prices = driver.find_elements(By.CLASS_NAME, "price")
+        descriptions = driver.find_elements(By.CLASS_NAME, "description")
+        ratings = driver.find_elements(By.CLASS_NAME, "ratings")
+
+        # Save results to the list
+        for i in range(len(titles)):
+            element_list.append([
+                titles[i].text,
+                prices[i].text,
+                descriptions[i].text,
+                ratings[i].text
+            ])
+        driver.quit()
+
+    # Display data
+    for row in element_list:
+        print(row)
+
 if __name__ == "__main__":
     main()
