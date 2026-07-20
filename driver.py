0846ff664ecebbdadcbb43f6764ceabec3833cf1
Erick <moralerick90@gmail.com>
2026-07-19 22:30:26 -0700

    options for chrome webdriver

--- driver.py
+++ driver.py
@@ -1,5 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 def main():
-    pass
+    element_list = []
+
+    # Set up Chrome options
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # Use a Service object
+    service = Service(ChromeDriverManager().install())
 
 if __name__ == "__main__":
     main()
