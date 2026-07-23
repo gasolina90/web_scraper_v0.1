66ca04bc9874e2ace9d4d287da56cd51dd3c487c
Erick <moralerick90@gmail.com>
2026-07-23 13:41:25 -0700

    init xml parser

---
+++ lxml.py
@@ -0,0 +1,11 @@
+# pip install virtualenv on global python interpreter and check version
+# In bottom right corner of VScode: > Select a Python environment > Quick Create
+# > Open Terminal > Check for green (.venv) preceeding file path
+
+# pip install beautifulsoup4, and requests
+
+from bs4 import BeautifulSoup
+import requests
+from lxml import etree
+
+url = "https://www.geeksforgeeks.org//"
